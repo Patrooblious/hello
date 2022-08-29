fc79ffa65c61fcaca6a3288c27425e285335186e
Patrick <pgurney2@huskers.unl.edu>
2022-08-29 21:43:38 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ *  Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello, my name is Patrick Gurney!\n")
+    printf("My Major is Computer Engineering!\n")
+
+    return 0;
+}

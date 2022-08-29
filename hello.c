20624f104d2d7ea41a6bbb12de5ce1e140cd7491
Patrick <pgurney2@huskers.unl.edu>
2022-08-29 21:53:12 +0000

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 /**
- *  Basic Hello World Program
+ * Basic Hello World Program
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
-    printf("Hello, my name is Patrick Gurney!\n")
-    printf("My Major is Computer Engineering!\n")
+     printf("Hello, my name is Patrick Gurney!\n");
+     printf("My Major is Computer Engineering!\n");
 
     return 0;
 }

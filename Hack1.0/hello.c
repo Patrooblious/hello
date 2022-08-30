89043eef9df307b16686fd7591283e43cc0f86da
Christopher Param <cparam2@huskers.unl.edu>
2022-08-30 00:01:36 +0000

    added my name, major and minor to the output

---
+++ Hack1.0/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Basic Hello World Program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+     printf("Hello, my name is Patrick Gurney!\n");
+     printf("My Major is Computer Engineering!\n");
+     printf("Hello, my name is Christopher Param!\n");
+     printf("My major is Electrical Engineering!\n");
+     printf("My minor is Mathematics!\n");
+
+    return 0;
+}
